3dd245f6c19a860bc446b3ff83e2987171dfa623
= <=>
2020-10-14 16:43:41 +0100

    add edit and delete functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 void main() => {runApp(MyApp())};
 
@@ -24,37 +25,34 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final _textTask = TextEditingController();
-  var _listOfTasks = [
-    "B",
-    "B",
-    "B",
-    "B",
-    "A",
-    "A",
-    "B",
-    "B",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "f",
-    "f",
-  ];
+  var _listOfTasks = [];
 
   @override
+
+  /// dispose method removes the widget
   void dispose() {
     _textTask.dispose();
     super.dispose();
   }
 
+  /// verification function for empty tasks
   void addToList(String _task) {
     if (_task.isNotEmpty || _task.length > 0) {
       setState(() => _listOfTasks.add(_task));
     }
+  }
+
+  /// Deletes item from the list
+  void _deleteItem(int index) {
+    setState(() => _listOfTasks.removeAt(index));
+  }
+
+  /// Put the item in the textbox and removes the task from the list to prevent duplicates
+  void _editItem(int index) {
+    setState(() {
+      _textTask.text = _listOfTasks[index].toString();
+      _listOfTasks.removeAt(index);
+    });
   }
 
   Widget buildViewTasks() {
@@ -62,10 +60,32 @@ class _HomeState extends State<Home> {
         shrinkWrap: true,
         itemCount: _listOfTasks.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_listOfTasks[index]),
-          );
+          return _getSliableListTiles(context, index);
         });
+  }
+
+  //Slidable Widget
+  Widget _getSliableListTiles(BuildContext context, int index) {
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      child: Container(
+        color: Colors.white,
+        child: ListTile(title: Text(_listOfTasks[index])),
+      ),
+      actions: <Widget>[
+        IconSlideAction(
+            caption: 'Edit',
+            color: Colors.blue,
+            icon: Icons.edit,
+            onTap: () => _editItem(index)),
+        IconSlideAction(
+            caption: 'Delete',
+            color: Colors.red,
+            icon: Icons.delete,
+            onTap: () => _deleteItem(index)),
+      ],
+    );
   }
 
   @override
@@ -85,7 +105,8 @@ class _HomeState extends State<Home> {
       ),
       floatingActionButton: FlatButton(
         onPressed: () {
-          addToList(_textTask.text);
+          addToList(_textTask.text); // add to list view
+          _textTask.text = ""; // remove the task from the text field
         },
         child: Icon(Icons.add),
       ),
